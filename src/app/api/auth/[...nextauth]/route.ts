a442588640fa882641f4f9d669e39d4a101f56ff
DESKTOP-6FVFHUO\기훈 <tjsrlgns97@gmail.com>
2026-07-08 01:35:18 +0900

    feat:NextAuth.js 백엔드 핸들러

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,28 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+
+const handler = NextAuth({
+  // 1. 로그인 파트너로 '구글'을 등록합니다.
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+  ],
+  // 2. 보안을 위한 비밀키 세팅
+  secret: process.env.NEXTAUTH_SECRET,
+  
+  // 3. (공부 포인트) 로그인 성공 시 유저 정보를 커스텀 가공하는 콜백 함수
+  callbacks: {
+    async session({ session, token }: any) {
+      if (session.user) {
+        session.user.id = token.sub; // 유저 고유 ID를 세션에 심어줍니다.
+      }
+      return session;
+    },
+  },
+});
+
+// Next.js가 GET 요청과 POST 요청 모두 이 핸들러로 처리하도록 내보냅니다.
+export { handler as GET, handler as POST };
