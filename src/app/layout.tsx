cba4259d9f7d95d60f83829013c49530e41be7c5
DESKTOP-6FVFHUO\기훈 <tjsrlgns97@gmail.com>
2026-07-08 01:45:17 +0900

    feat: AuthContext layout.tsx에 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import AuthContext from "./api/auth/AuthContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +25,12 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="ko"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <AuthContext>{children}</AuthContext>
+      </body>
     </html>
   );
 }
