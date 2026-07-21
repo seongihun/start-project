7730ad0ce35f00965f13b48b2116fdae726eb2e2
DESKTOP-6FVFHUO\기훈 <tjsrlgns97@gmail.com>
2026-07-22 00:36:08 +0900

    이미지 도메인 허용설정 추가

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+
+  // 💡 구글 프로필 이미지 도메인 허용 설정 추가
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
